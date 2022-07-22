3a483aec91d8073d83ff3748b8ec3878c90c9c20
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-07-22 09:16:46 +0500

    new problem

---
+++ LeetCodeProblems/Remove Element.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var array = [0,1,2,2,3,0,4,2]
+let val = 2
+
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var k = 0
+    for i in 0..<nums.count {
+        if nums[i] != val {
+            nums[k] = nums[i]
+            k += 1
+        }
+    }
+    return k
+}
+print(removeElement(&array, val))
