41ff98661c4d2a73cbc751e97ed9eadc13468b64
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-08-07 09:38:21 +0500

    Add binary

--- LeetCodeProblems/main.swift
+++ LeetCodeProblems/main.swift
@@ -1,32 +1,42 @@
 import Foundation
 
-let digits = [8, 9, 9]
+let a = "1010"
+var b = "1011"
 
-func plusOne(_ digits: [Int]) -> [Int] {
-
-    var result = digits
-    var index = result.count - 1
-    
-    while result[index] == 9 {
-        result[index] = 0
-        index -= 1
-        
-        if index < 0 {
+func addBinary(_ a: String, _ b: String) -> String {
+    let la = a.count
+    let lb = b.count
+    var aArr = Array(a)
+    var bArr = Array(b)
+    if la > lb {
+        bArr = Array(repeating: "0", count: (la - lb)) + bArr
+    } else {
+        aArr = Array(repeating: "0", count: (lb - la)) + aArr
+    }
+    var result = [Int]()
+    var carry = 0
+    for i in stride(from: aArr.count - 1, to: -1, by: -1) {
+        let curr = Int(String(aArr[i]))! + Int(String(bArr[i]))! + carry
+        carry = 0
+        if curr < 2 {
+            result.insert(curr, at: 0)
+        } else if curr < 3 {
+            result.insert(0, at: 0)
+            carry = 1
+        } else {
             result.insert(1, at: 0)
-            break
+            carry = 1
         }
     }
-    
-    if index >= 0 {
-        result[index] += 1
+    if carry != 0 {
+        result.insert(carry, at: 0)
     }
+    let otv = result.map { String ($0) }
     
-
-    return result
-
-    
+    print(aArr)
+    print(bArr)
+    return otv.joined(separator: "")
 }
 
-print(plusOne(digits))
-
+print(addBinary(a, b))
 
