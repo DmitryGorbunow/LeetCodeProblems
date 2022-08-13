48e0c5bfd09bc1460d55fadf7e03ecc189a6a5d7
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-08-13 21:52:18 +0500

    Climbing Stairs

--- LeetCodeProblems/main.swift
+++ LeetCodeProblems/main.swift
@@ -1,42 +1,24 @@
-import Foundation
+//func fib(i: Int) -> Int {
+//    if i < 2 { return 1 }
+//    else { return fib(i: i - 1) + fib(i: i - 2)}
+//}
+//
+//print(fib(i: 45))
 
-let a = "1010"
-var b = "1011"
 
-func addBinary(_ a: String, _ b: String) -> String {
-    let la = a.count
-    let lb = b.count
-    var aArr = Array(a)
-    var bArr = Array(b)
-    if la > lb {
-        bArr = Array(repeating: "0", count: (la - lb)) + bArr
-    } else {
-        aArr = Array(repeating: "0", count: (lb - la)) + aArr
-    }
-    var result = [Int]()
-    var carry = 0
-    for i in stride(from: aArr.count - 1, to: -1, by: -1) {
-        let curr = Int(String(aArr[i]))! + Int(String(bArr[i]))! + carry
-        carry = 0
-        if curr < 2 {
-            result.insert(curr, at: 0)
-        } else if curr < 3 {
-            result.insert(0, at: 0)
-            carry = 1
+func fib(i: Int) -> Int {
+    var arr = [Int]()
+    for k in 0...46 {
+        if k > 1 {
+            arr.append(arr[k-1] + arr[k-2])
+        } else if k == 1 {
+            arr.append(1)
         } else {
-            result.insert(1, at: 0)
-            carry = 1
+            arr.append(0)
         }
     }
-    if carry != 0 {
-        result.insert(carry, at: 0)
-    }
-    let otv = result.map { String ($0) }
-    
-    print(aArr)
-    print(bArr)
-    return otv.joined(separator: "")
+    return arr[i + 1]
 }
 
-print(addBinary(a, b))
+print(fib(i: 45))
 
