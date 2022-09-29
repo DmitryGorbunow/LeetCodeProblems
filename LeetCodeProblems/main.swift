a5124778ed0a2085b3b7f1a179e7abda1f2b0e97
DmitryGorbunow <84672242+DmitryGorbunow@users.noreply.github.com>
2022-09-29 10:56:36 +0300

    new

--- LeetCodeProblems/main.swift
+++ LeetCodeProblems/main.swift
@@ -1,24 +1,28 @@
 import Foundation
 
-class Solution {
-    func isAnagram(_ s: String, _ t: String) -> Bool {
-        var dict1 = [Character: Int]()
-        var dict2 = [Character: Int]()
+protocol MyDelegate {
+    func my(number: Int)
+}
 
-        let baseCounts1 = zip(
-            s, repeatElement(1,count: Int.max))
-        dict1 = Dictionary(baseCounts1, uniquingKeysWith: +)
-        
-        let baseCounts2 = zip(
-            t, repeatElement(1,count: Int.max))
-        dict2 = Dictionary(baseCounts2, uniquingKeysWith: +)
-        
-        return dict1 == dict2
+
+class My: MyDelegate {
+    func my(number: Int) {
+        print("My number = \(number)")
     }
 }
 
-let solution = Solution()
+class You {
+    var delegate: MyDelegate?
+    
+    func newMy() {
+        delegate?.my(number: 10)
+    }
+}
 
-print(solution.isAnagram("anagram", "nagaram"))
+let my = My()
+let you = You()
+you.delegate = my
 
+you.newMy()
+// hello
 
